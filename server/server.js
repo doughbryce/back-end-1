e94ad510c2e675cfa03cefc5fdb13b94e9386b19
doughbryce <31143323+doughbryce@users.noreply.github.com>
2022-05-02 14:45:20 -0600

    final

--- server/server.js
+++ server/server.js
@@ -6,4 +6,20 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(4000, () => console.log(`server running on port 4000`));
+let friends = [`nitin`, `cameron`, `jack`, `ron`, `riley`, `jeddy`, `harry`, `jess`, `lily`, `hermoine`];
+
+app.get(`/api/users`, (req, res) => {
+    res.status(200).send(friends)
+});
+
+app.get(`/weather/:temperature`, (req, res) => {
+    const { temperature } = req.params;
+    const phrase = `<h3>It was ${temperature} today</h3>`;
+    res.status(200).send(phrase);
+})
+
+
+
+let SERVER_PORT = 4000;
+
+app.listen(SERVER_PORT, () => console.log(`server running on port 4000`));
